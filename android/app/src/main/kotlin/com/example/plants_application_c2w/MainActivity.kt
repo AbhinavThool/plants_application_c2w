555d8b0c8aff1a6bac0a1629dcef07277832bc05
Abhinav Thool <abhinavthool@gmail.com>
2024-04-03 23:53:45 +0530

    get started page done

---
+++ android/app/src/main/kotlin/com/example/plants_application_c2w/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.plants_application_c2w
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/get_started.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GetStartedScreen extends StatefulWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  State<GetStartedScreen> createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(251, 247, 248, 1),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //image
+            Image.asset(
+              'assets/images/get_started_plant.png',
+              width: double.infinity,
+            ),
+
+            // text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Enjoy your',
+                      style: GoogleFonts.poppins(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 5,
+                        color: const Color.fromRGBO(47, 47, 47, 1),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'life with',
+                          style: GoogleFonts.poppins(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 5,
+                            color: const Color.fromRGBO(47, 47, 47, 1),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Text(
+                          'Plants',
+                          style: GoogleFonts.poppins(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 5,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            //spacing
+            const SizedBox(height: 50),
+
+            // button
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color.fromRGBO(62, 102, 24, 1),
+                      Color.fromRGBO(124, 180, 70, 1),
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      offset: Offset(0, 20),
+                      blurRadius: 20,
+                      color: Color.fromRGBO(0, 0, 0, 0.15),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                width: 320,
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Get Started",
+                      style: GoogleFonts.rubik(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    const Icon(
+                      Icons.arrow_forward_ios,
+                      size: 24,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            // end
+          ],
+        ),
+      ),
+    );
+  }
+}
